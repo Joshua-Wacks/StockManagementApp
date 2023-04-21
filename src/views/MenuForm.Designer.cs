23c813fd7263385a0e02edb9eba77dbfddb77242
Joshua-Wacks <joshuawa@bdd.co.za>
2023-04-21 07:17:25 +0200

    Menu Added

---
+++ src/views/MenuForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace StockManagementApp
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnViewStock = new System.Windows.Forms.Button();
+            this.btnUpdateStock = new System.Windows.Forms.Button();
+            this.btnStats = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(242, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stock Management System";
+            // 
+            // btnViewStock
+            // 
+            this.btnViewStock.Location = new System.Drawing.Point(62, 162);
+            this.btnViewStock.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnViewStock.Name = "btnViewStock";
+            this.btnViewStock.Size = new System.Drawing.Size(232, 60);
+            this.btnViewStock.TabIndex = 1;
+            this.btnViewStock.Text = "View Stock";
+            this.btnViewStock.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateStock
+            // 
+            this.btnUpdateStock.Location = new System.Drawing.Point(344, 162);
+            this.btnUpdateStock.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdateStock.Name = "btnUpdateStock";
+            this.btnUpdateStock.Size = new System.Drawing.Size(232, 60);
+            this.btnUpdateStock.TabIndex = 2;
+            this.btnUpdateStock.Text = "Update Stock";
+            this.btnUpdateStock.UseVisualStyleBackColor = true;
+            // 
+            // btnStats
+            // 
+            this.btnStats.Location = new System.Drawing.Point(625, 162);
+            this.btnStats.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(232, 60);
+            this.btnStats.TabIndex = 3;
+            this.btnStats.Text = "Analyse Stock";
+            this.btnStats.UseVisualStyleBackColor = true;
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 338);
+            this.Controls.Add(this.btnStats);
+            this.Controls.Add(this.btnUpdateStock);
+            this.Controls.Add(this.btnViewStock);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MenuForm";
+            this.Text = "Stock Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnViewStock;
+        private Button btnUpdateStock;
+        private Button btnStats;
+    }
+}
---
+++ src/views/MenuForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockManagementApp
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewStock_Click(object sender, EventArgs e)
+        {
+            ViewDataForm viewDataForm = new ViewDataForm();
+            viewDataForm.ShowDialog();
+        }
+
+        private void btnStats_Click(object sender, EventArgs e)
+        {
+            GraphForm graphForm = new GraphForm();
+            graphForm.ShowDialog();
+        }
+    }
+}
